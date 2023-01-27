966c0af8e6e542b8db51beacbe7772797780f3fc
TomasAres <aresjtomas@gmail.com>
2023-01-27 15:05:29 -0300

    Request a demo two columns

--- components/request-demo.php
+++ components/request-demo.php
@@ -1,20 +1,23 @@
-<section class="container-fluid bg-black bottom-xxl" id="demo">
-   <div class="container">
-      <div>
-         <div class="row d-flex align-items-center justify-content-center">
-            <div class="col-md-8">
-               <div class="text-center">
-                  <h3 class="semibold white bottom-lg">The right solution for your business</h3>  
-                  <p class="home-text">CONNECTED offers a wide range of integration services, allowing you to easily connect your existing systems and data to our platform.</p>
-                  <p class="home-text">
-                  In adition, our platform is highly customizable to meet the unique needs of each client. Our team can work with you to tailor the system to your exact requirements.
-                  </p>
-                  <div class="button-wrapper top-xl"><a class="button big" href="/contact.php">Request a demo</a></div>
-
-               </div>
-            </div>
-            </div>
+<section class="container-fluid bg-primary bottom-xxl top-xxl" id="demo">
+   <div class="container text-center">
+      <div class="row justify-content-center g-5">
+         <div class="col-md-8">
+            <h3 class="semibold bottom-xl">The right solution for your business</h3>
+         </div>
+         <div class="w-100"></div>
+         <div class="col-md-6">
+            <img src="img/icons/api_icon.svg" alt="API icon" class="service-icon" height="40px">
+            <p class="home-text">CONNECTED offers a wide range of integration services, allowing you to easily connect your existing systems and data to our platform.</p>
+         </div>
+         <div class="col-md-6">
+            <img src="img/icons/coding_icon.svg" alt="Coding icon" class="service-icon" height="40px">
+            <p class="home-text">
+               In adition, our platform is highly customizable to meet the unique needs of each client. Our team can work with you to tailor the system to your exact requirements.
+            </p>
          </div>
       </div>
+      <div class="button-wrapper top-xl"><a class="button big" href="/contact.php">Request a demo</a></div>
+   </div>
+   </div>
    </div>
 </section>
